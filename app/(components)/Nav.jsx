a16d5004fe42baf1c13fd18b10e3e10abd8cbd80
yadhu1335 <yadhu1335@gmail.com>
2024-02-14 11:57:47 +0530

    added Nav.jsx and added  it to layout

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,8 @@
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default nav;
+
+//The use of component folder is that we cannot navigate to that unlike ticket page. So we store
+//files such as borders, etc that we dont want to be navigated.
