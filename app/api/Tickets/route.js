a91a9b7f3ed4f48fc992c2ea47b4456e09ceefb8
yadhu1335 <yadhu1335@gmail.com>
2024-02-26 11:06:14 +0530

    First commit of the day. Added filtering based on categories

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,4 +1,4 @@
-import Ticket from "../../(models)/Ticket";
+import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server"; //NextResponse is an API in Next. js that allows you to work with responses in Middleware. It provides methods for redirecting requests, rewriting responses, setting headers, cookies, etc.
 
 export async function POST(req) {
@@ -9,6 +9,15 @@ export async function POST(req) {
     await Ticket.create(ticketData); //.create is a mongoose function
 
     return NextResponse.json({ message: "Ticket Succesfull" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
---
+++ app/page.jsx
@@ -0,0 +1,46 @@
+import TicketCard from "./(components)/TicketCard";
+
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+  }
+};
+
+const Dashbord = async () => {
+  const { tickets } = await getTickets();
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)), //TO remove any duplicates
+  ];
+
+  return (
+    <div className="p-5 ">
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {/* the above div is to make it suitable for both small screens and large screens */}
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dashbord;
